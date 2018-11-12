3f8177b507547e57a51846da1fb4549fc18365c8
Marcus Ahnve <marcus@ahnve.com>
2018-11-12 23:25:19 +0100

    initial

---
+++ Hur man lyckas med systemutveckling.mm
@@ -0,0 +1,162 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Hur man lyckas med systemutveckling" FOLDED="false" ID="ID_226466942" CREATED="1542031779313" MODIFIED="1542031908720" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false" show_note_icons="true" show_icon_for_attributes="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="11" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="&#xc4;r ni bra p&#xe5; systemutveckling" POSITION="right" ID="ID_1375576568" CREATED="1542032209600" MODIFIED="1542032246371">
+<edge COLOR="#00ffff"/>
+</node>
+<node TEXT="Vad inneb&#xe4;r att lyckas?" POSITION="right" ID="ID_1278262412" CREATED="1542031795797" MODIFIED="1542031810251">
+<edge COLOR="#ff0000"/>
+<node TEXT="Projekt" ID="ID_780242839" CREATED="1542031918642" MODIFIED="1542031924987">
+<node TEXT="Inga int&#xe4;kter" ID="ID_926126775" CREATED="1542031940832" MODIFIED="1542031944137"/>
+</node>
+<node TEXT="Produkt" ID="ID_176132947" CREATED="1542031925787" MODIFIED="1542031928144"/>
+<node TEXT="Vanliga fel i &quot;lyckas&quot;" ID="ID_22374015" CREATED="1542058779572" MODIFIED="1542061093110">
+<node TEXT="LOC" ID="ID_866031131" CREATED="1542058861792" MODIFIED="1542058863891"/>
+<node TEXT="Features" ID="ID_1694629633" CREATED="1542058797481" MODIFIED="1542058801828"/>
+<node TEXT="Cycle time" ID="ID_880194890" CREATED="1542058805916" MODIFIED="1542058814666"/>
+<node TEXT="Percent done" ID="ID_75191693" CREATED="1542058826092" MODIFIED="1542058832933"/>
+<node TEXT="Story Points" ID="ID_474888126" CREATED="1542058833495" MODIFIED="1542058842262">
+<node TEXT="Velocity" ID="ID_409438616" CREATED="1542058802430" MODIFIED="1542058805411"/>
+</node>
+</node>
+<node TEXT="Hur vet vi om vi lyckas?" ID="ID_1345875352" CREATED="1542032068654" MODIFIED="1542061106882">
+<node TEXT="Hitta m&#xe4;tpunkt" ID="ID_839115284" CREATED="1542032117723" MODIFIED="1542032128694"/>
+<node TEXT="M&#xe4;t ofta" ID="ID_1954997087" CREATED="1542032130078" MODIFIED="1542032132947"/>
+<node TEXT="Det som m&#xe4;ts - sista kolumn p&#xe5; tavlan" ID="ID_696158379" CREATED="1542058974722" MODIFIED="1542058987525"/>
+<node TEXT="Build-learn-measure" ID="ID_759953102" CREATED="1542058943757" MODIFIED="1542058950312"/>
+</node>
+</node>
+<node TEXT="Hitta relevanta m&#xe4;tv&#xe4;rden" POSITION="left" ID="ID_1534475688" CREATED="1542031972773" MODIFIED="1542031992029">
+<edge COLOR="#0000ff"/>
+<node TEXT="" ID="ID_661045478" CREATED="1542032019168" MODIFIED="1542032019168"/>
+</node>
+<node TEXT="R&#xe4;kna inte features" POSITION="left" ID="ID_1855605279" CREATED="1542032030352" MODIFIED="1542032037835">
+<edge COLOR="#00ff00"/>
+</node>
+<node TEXT="Projekt" POSITION="left" ID="ID_936741175" CREATED="1542059046543" MODIFIED="1542059049353">
+<edge COLOR="#00007c"/>
+<node TEXT="Inga int&#xe4;kter" ID="ID_1177553864" CREATED="1542059060889" MODIFIED="1542059064930"/>
+</node>
+<node TEXT="L&#xe4;ttr&#xf6;rlig teknik" POSITION="left" ID="ID_286436444" CREATED="1542059088999" MODIFIED="1542059097211">
+<edge COLOR="#7c007c"/>
+<node TEXT="Simple vs Easy" ID="ID_1519102317" CREATED="1542059098271" MODIFIED="1542059105281"/>
+<node TEXT="Hur l&#xe5;ng &#xe4;r feedbackloopen?" ID="ID_1352686789" CREATED="1542059152598" MODIFIED="1542059163119">
+<node TEXT="4w" ID="ID_336216958" CREATED="1542059165152" MODIFIED="1542059172193"/>
+<node TEXT="10min" ID="ID_872982723" CREATED="1542059172691" MODIFIED="1542059207959"/>
+<node TEXT="10sec" ID="ID_1349140781" CREATED="1542059187638" MODIFIED="1542059214876"/>
+</node>
+<node TEXT="Hur ser vi att koden fungerar" ID="ID_1621885892" CREATED="1542059235690" MODIFIED="1542059245959">
+<node TEXT="OO" ID="ID_520482200" CREATED="1542059248040" MODIFIED="1542059250197">
+<node TEXT="Tester kr&#xe4;vs f&#xf6;r att k&#xf6;ra koden &#xf6;ht" ID="ID_1188111917" CREATED="1542059250203" MODIFIED="1542059260906"/>
+</node>
+<node TEXT="FP" ID="ID_17085331" CREATED="1542059263639" MODIFIED="1542059266388">
+<node TEXT="Enkelt att testa" ID="ID_1887700034" CREATED="1542059266395" MODIFIED="1542059270592"/>
+</node>
+</node>
+<node TEXT="Deployability" ID="ID_1710706063" CREATED="1542059310515" MODIFIED="1542059320788">
+<node TEXT="DevOps" ID="ID_741198499" CREATED="1542060980635" MODIFIED="1542060983879"/>
+<node TEXT="Deploying should not be scary" ID="ID_1188049223" CREATED="1542061017794" MODIFIED="1542061026787"/>
+</node>
+<node TEXT="Design for replacability" ID="ID_1875555507" CREATED="1542059436238" MODIFIED="1542059443368">
+<node TEXT="Automation of processes" ID="ID_841459453" CREATED="1542059635411" MODIFIED="1542059648122"/>
+<node TEXT="Design around communication" ID="ID_649284469" CREATED="1542060685664" MODIFIED="1542060697477">
+<node TEXT="Not implementation" ID="ID_35155763" CREATED="1542060702023" MODIFIED="1542060706348">
+<node TEXT="Std implementation stops simplest tech" ID="ID_1689202984" CREATED="1542060749139" MODIFIED="1542060761704"/>
+</node>
+</node>
+<node TEXT="Lifecycle includes death" ID="ID_369859086" CREATED="1542060798959" MODIFIED="1542060811558">
+<node TEXT="Otherwise cost for replacing is placed on system replacing" ID="ID_561447139" CREATED="1542060816219" MODIFIED="1542060836826"/>
+</node>
+<node TEXT="Smaller parts" ID="ID_683224062" CREATED="1542060855636" MODIFIED="1542060858499"/>
+</node>
+<node TEXT="Avoid Frameworks" ID="ID_87648411" CREATED="1542059332707" MODIFIED="1542059337560">
+<node TEXT="Aggregation over inheritance" ID="ID_1459382230" CREATED="1542059343274" MODIFIED="1542059351158"/>
+<node TEXT="And frameworks" ID="ID_924857726" CREATED="1542060668434" MODIFIED="1542060674356"/>
+</node>
+<node TEXT="Avoid shared libraries" ID="ID_185103725" CREATED="1542059362712" MODIFIED="1542059368242">
+<node TEXT="Version hell" ID="ID_898858067" CREATED="1542059370306" MODIFIED="1542059377139"/>
+<node TEXT="Uberrepo" ID="ID_859143037" CREATED="1542059405797" MODIFIED="1542059411459"/>
+</node>
+</node>
+<node TEXT="Organisation" POSITION="left" ID="ID_547955928" CREATED="1542059446779" MODIFIED="1542059449709">
+<edge COLOR="#007c7c"/>
+<node TEXT="Conways lag" ID="ID_1765852221" CREATED="1542059450952" MODIFIED="1542059454241">
+<node TEXT="Mappa efter services" ID="ID_395634835" CREATED="1542060102114" MODIFIED="1542060108385">
+<node TEXT="Low coupling - high cohesion" ID="ID_1571458375" CREATED="1542060111953" MODIFIED="1542060121001"/>
+</node>
+<node TEXT="Minimera behov av kommunikation" ID="ID_1384555929" CREATED="1542060142282" MODIFIED="1542060159387"/>
+</node>
+<node TEXT="Str&#xe4;va mot t-kompetenser" ID="ID_728651441" CREATED="1542059497707" MODIFIED="1542059506326"/>
+<node TEXT="Feedback" ID="ID_1566891239" CREATED="1542059507416" MODIFIED="1542059512598">
+<node TEXT="Retrospektiv" ID="ID_980241498" CREATED="1542059514080" MODIFIED="1542059517135"/>
+</node>
+</node>
+<node TEXT="We&apos;re not an IT-company" POSITION="right" ID="ID_1781057661" CREATED="1542059598205" MODIFIED="1542059607057">
+<edge COLOR="#7c7c00"/>
+<node TEXT="Neither is Facebook" ID="ID_444806946" CREATED="1542059608476" MODIFIED="1542059613633"/>
+</node>
+</node>
+</map>
